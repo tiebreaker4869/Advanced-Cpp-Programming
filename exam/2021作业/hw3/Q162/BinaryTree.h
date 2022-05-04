edb186a08e0df0cda7192483fbdd00e2d53cbd46
SUNLIFAN <1656680420@qq.com>
2022-05-04 11:17:08 +0800

    add ps

---
+++ exam/2021作业/hw3/Q162/BinaryTree.h
@@ -0,0 +1,23 @@
+﻿#ifdef BINARYTREE_EXPORTS
+#define BINARYTREE_API __declspec(dllexport)
+#else
+#define BINARYTREE_API __declspec(dllimport)
+#endif
+
+#include <iostream>
+
+namespace cpp {
+	struct BINARYTREE_API TreeNode {
+	public:
+		int data;
+		TreeNode *left, *right;
+
+		TreeNode(void);
+		TreeNode(int data);
+
+	private:
+		TreeNode(int data, TreeNode *left, TreeNode *right);
+	};
+
+	BINARYTREE_API void print_tree(const TreeNode *root);
+}
