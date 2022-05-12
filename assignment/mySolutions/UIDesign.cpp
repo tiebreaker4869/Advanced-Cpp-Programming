be307fa5f4d5320155d41a8ec8a7f7f535f40b6f
SUNLIFAN <1656680420@qq.com>
2022-05-12 16:37:35 +0800

    finish UIDesign

--- assignment/mySolutions/UIDesign.cpp
+++ assignment/mySolutions/UIDesign.cpp
@@ -5,109 +5,114 @@
 #include <unordered_map>
 
 class Color {
-    private:
-        int r;
-        int g;
-        int b;
-    public:
-        Color():r(0),g(0),b(0){}
+private:
+    int r;
+    int g;
+    int b;
+public:
+    Color():r(0),g(0),b(0){}
 
-        Color(const Color& c){
-            this->r = c.get_r();
-            this->g = c.get_g();
-            this->b = c.get_b();
-        }
-        
-        int get_r() const{return r;}
-        int get_g() const{return g;}
-        int get_b() const{return b;}
+    Color(const Color& c){
+        this->r = c.get_r();
+        this->g = c.get_g();
+        this->b = c.get_b();
+    }
 
-        void set_color(int r, int g, int b){
-            this->r = r;
-            this->g = g;
-            this->b = b;
-        }
+    int get_r() const{return r;}
+    int get_g() const{return g;}
+    int get_b() const{return b;}
 
-        void set_color(Color& c){
-            this->r = c.get_r();
-            this->g = c.get_g();
-            this->b = c.get_b();
-        }
+    void set_color(int r, int g, int b){
+        this->r = r;
+        this->g = g;
+        this->b = b;
+    }
+
+    void set_color(Color& c){
+        this->r = c.get_r();
+        this->g = c.get_g();
+        this->b = c.get_b();
+    }
 };
 
 class Rectangle {
-    protected:
-        Color color;
-    public:
-        std::string polyid;
-        Rectangle(std::string id):color(Color()),polyid(id){}
-        virtual void set_color(Color c)=0;
-        int get_r()const {return color.get_r();}
-        int get_g()const {return color.get_g();}
-        int get_b()const {return color.get_b();}
-        std::string get_id() const{return polyid;}
+protected:
+    Color color;
+public:
+    std::string polyid;
+    Rectangle(std::string id):color(Color()),polyid(id){}
+    virtual void set_color(Color c)=0;
+    int get_r()const {return color.get_r();}
+    int get_g()const {return color.get_g();}
+    int get_b()const {return color.get_b();}
+    std::string get_id() const{return polyid;}
 };
 
 class Normal : public Rectangle {
-    public:
-        Normal(std::string id):Rectangle(id){}
-        void set_color(Color c) override {
-            color.set_color(c);
-        }
+public:
+    Normal(std::string id):Rectangle(id){}
+    void set_color(Color c) override {
+        color.set_color(c);
+    }
 };
 
 class Reverse : public Rectangle {
-    public:
-        Reverse(std::string id):Rectangle(id){}
-        void set_color(Color c) override {
-            color.set_color(255-c.get_r(), 255-c.get_g(), 255-c.get_b());
-        }
+public:
+    Reverse(std::string id):Rectangle(id){}
+    void set_color(Color c) override {
+        color.set_color(255-c.get_r(), 255-c.get_g(), 255-c.get_b());
+    }
 };
 
 class Single : public Rectangle {
-    public:
-        Single(std::string id):Rectangle(id){}
-        void set_color(Color c) override {
-            color.set_color(c.get_r(), 0, 0);
-        }
+public:
+    Single(std::string id):Rectangle(id){}
+    void set_color(Color c) override {
+        color.set_color(c.get_r(), 0, 0);
+    }
 };
 
 class RectangleFactory {
-    public:
-        static Rectangle* createRectangle(std::string type, std::string id);
+public:
+    static Rectangle* createRectangle(std::string type, std::string id);
 };
 
 Rectangle* RectangleFactory::createRectangle(std::string type, std::string id){
-            if(type == "normal")
-                return new Normal(id);
-            if(type == "single")
-                return new Single(id);
-            if(type == "reverse")
-                return new Reverse(id);
-        
-        std::cout << "invalid type" << std::endl;
-        
-        return nullptr;
+    if(type == "normal")
+        return new Normal(id);
+    if(type == "single")
+        return new Single(id);
+    if(type == "reverse")
+        return new Reverse(id);
+
+    std::cout << "invalid type" << std::endl;
+
+    return nullptr;
 }
 
 void print(const std::vector<Rectangle*>& recs){
     for(int i = 0; i < recs.size(); i ++){
         std::cout << recs[i]->get_id() << " " << recs[i]->get_r() << " " << recs[i]->get_g() << " "
-        << recs[i]->get_b() << std::endl;
+                  << recs[i]->get_b();
+        if(i != recs.size()-1)std::cout << std::endl;
     }
 }
 
-inline int calculate_gray_val(const int r,const int g, const int b){
+inline double calculate_gray_val(const double r,const double g, const double b){
     return r * 0.299 + g * 0.587 + b * 0.114;
 }
 
 bool cmp_by_id(const Rectangle* rec1, const Rectangle* rec2){
-    return rec1->get_id() < rec2->get_id();
+    return std::stoi(rec1->get_id().substr(1)) < std::stoi(rec2->get_id().substr(1));
 }
 
 bool cmp_by_gray(const Rectangle* rec1, const Rectangle* rec2){
-    return calculate_gray_val(rec1->get_r(), rec1->get_g(), rec1->get_b()) < 
-        calculate_gray_val(rec2->get_r(), rec2->get_g(), rec2->get_b());   
+    double gray_val1 = calculate_gray_val(rec1->get_r(), rec1->get_g(), rec1->get_b());
+    double gray_val2 = calculate_gray_val(rec2->get_r(), rec2->get_g(), rec2->get_b());
+    if(gray_val1 < gray_val2)return true;
+    if(gray_val1 > gray_val2)return false;
+
+    return std::stoi(rec1->get_id().substr(1) ) < std::stoi(rec2->get_id().substr(1));
 }
 
 int main(){
@@ -125,7 +130,7 @@ int main(){
             recs.push_back(rec);
         }else if(cmd == "Group"){
             int n;
-            std::vector<std::string> indices; 
+            std::vector<std::string> indices;
             std::cin >> n;
             indices.resize(n);
             for(int i = 0; i < n; i ++){
@@ -134,8 +139,8 @@ int main(){
             std::string groupId;
             std::cin >> groupId;
             if(group_record.count(groupId) == 0)group_record[groupId] = std::vector<Rectangle*>();
-            for(int i = 0; i < recs.size(); i ++){
-                for(int j = 0; j < indices.size(); j ++){
+            for(int j = 0; j < indices.size(); j ++){
+                for(int i = 0; i < recs.size(); i ++){
                     if(indices[j] == recs[i]->get_id()){
                         group_record[groupId].push_back(recs[i]);
                         break;
@@ -144,9 +149,9 @@ int main(){
             }
         }else if(cmd == "Set"){
             std::string polyid;
-            int r, g, b; 
-            std::cin >> polyid >> r >> g >> b; 
-            Color c; 
+            int r, g, b;
+            std::cin >> polyid >> r >> g >> b;
+            Color c;
             c.set_color(r, g, b);
             if(polyid[0] == 'P') {
                 for (int i = 0; i < recs.size(); i++) {
